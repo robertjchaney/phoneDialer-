2c39a97b56cc2e79ffa8dbbc8c09622a3df34863
robertjchaney <rchaney8384@stu.neisd.net>
2023-09-18 11:40:13 -0500

    9/18/2023

--- phoneDialer.java
+++ phoneDialer.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
 import javax.swing.border.*;
 import java.awt.*;
+import java.awt.event.*;
+
 // Add a new class also called PhoneDialer.  Be sure to create a main() method.
-public class phoneDialer {
+// Make your PhoneDialer class implement the ActionListener interface to handle the button clicks.
+public class phoneDialer implements ActionListener{
     // As PhoneDialer class members, declare JButton variables for all numbers between 0 and 9
     JButton zero = new JButton("0");
     JButton one = new JButton("1");
@@ -36,6 +39,7 @@ public class phoneDialer {
         JPanel mainPanel = (JPanel)dialerFrame.getContentPane();
         // Set the layout to a new BoxLayout with vertical orientation
         BoxLayout myLayout = new BoxLayout(mainPanel, BoxLayout.Y_AXIS);
+        mainPanel.setLayout(myLayout);
         //Set a new Border of empty space with 10 pixels on all sides
         mainPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
 
@@ -46,16 +50,17 @@ public class phoneDialer {
         topPanel.setLayout(layout);
         // Create a new JLabel saying "Enter the number to dial:".  Add it to your label JPanel.
         JLabel theLabel = new JLabel("Enter the number to dial: ");
-        dialerFrame.add(theLabel);
+        topPanel.add(theLabel);
 
         //Create a new JPanel to hold your grid of 0-9 and dash buttons
         JPanel gridPanel = new JPanel();
         // Set the layout to a new GridLayout with 4 rows, 3 columns, and spacing of 5 pixels between cells both horizontally and vertically
         GridLayout GL = new GridLayout(4, 3, 5, 5);
+        gridPanel.setLayout(GL);
         // Set a new Border of empty space with 5 pixels on all sides
         gridPanel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
         // Add each of the new JButtons to your grid in an order that arranges them as shown in the "Project Details" section above.
-        gridPanel.add(zero);
+        
         gridPanel.add(one);
         gridPanel.add(two);
         gridPanel.add(three);
@@ -65,6 +70,7 @@ public class phoneDialer {
         gridPanel.add(seven);
         gridPanel.add(eight);
         gridPanel.add(nine);
+        gridPanel.add(zero);
         gridPanel.add(dash);
 
         // Now add your new child panel holding the label to your main content panel
@@ -73,11 +79,44 @@ public class phoneDialer {
         mainPanel.add(gridPanel);
         // Create a new JButton with the text "Dial Number" and store it as a class member variable
         JButton dialButton = new JButton("Dial Number");
+        // Set the X alignment of the JButton to Component.CENTER_ALIGNMENT.
         dialButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        // Add the new JButton directly to your main JPanel under the other two child panels
         mainPanel.add(dialButton);
-
+        // Finally, call setVisible(true) on your main frame to make the window visible!
         dialerFrame.setVisible(true);
 
+        // Call addActionListener() on all of your JButton objects, passing in the "this" parameter to make your main class the recipient of all action events from those buttons.
+        dialButton.addActionListener(this);
+    }
+    // Add the actionPerformed() method as defined on the ActionListener interface.
+    public void actionPerformed(ActionEvent event){
+        Object input = event.getSource();
+        // Use JOptionPane.showMessageDialog() to show the phone number currently stored in the class-level phone number String
+        JOptionPane.showMessageDialog(null, "Dialing: " + phoneNumber);
+        // Add a series of else if() statements, checking for a match on each remaining button representing a digit or dash on the keypad.
+        if(input == one)
+            phoneNumber += "1";
+        else if(input == two)
+            phoneNumber += "2";
+        else if(input == three)
+            phoneNumber += "3";
+        else if(input == four)
+            phoneNumber += "4";
+        else if(input == five)
+            phoneNumber += "5";
+        else if(input == six)
+            phoneNumber += "6";
+        else if(input == seven)
+            phoneNumber += "7";
+        else if(input == eight)
+            phoneNumber += "8";
+        else if(input == nine)
+            phoneNumber += "9";
+        else if(input == zero)
+            phoneNumber += "0";
+        else if(input == dash)
+            phoneNumber += "-";
+        }
 
     }
-}
